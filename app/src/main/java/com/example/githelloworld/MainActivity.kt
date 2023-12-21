e66e4fc1ffc5236fae11db223a6ec0038a222030
Vladislav Izotov <vladislavizotov@MacBook-Pro-Vladislav.local>
2023-12-21 20:19:52 +0200

    Add toast

--- app/src/main/java/com/example/githelloworld/MainActivity.kt
+++ app/src/main/java/com/example/githelloworld/MainActivity.kt
@@ -1,5 +1,17 @@
 package com.example.githelloworld
 
-class MainActivity {
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
+class MainActivity:Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        val button:Button = findViewById(R.id.myButton)
+        button.setOnClickListener {
+            Toast.makeText(this,"Кнопка натиснута", Toast.LENGTH_SHORT).show()
+        }
+    }
 }
